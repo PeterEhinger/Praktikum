c8358bfbf8ff4684e343576d1207a062f8b7c7e1
PeterEhinger <pehinger95@gmail.com>
2020-02-01 14:25:03 +0100

    Test

---
+++ src/edu/unibw/se/gdp/praktikum03/Quader.java
@@ -0,0 +1,31 @@
+package edu.unibw.se.gdp.praktikum03;
+
+import edu.unibw.se.gdp.graphics.SimpleGraphicPanel;
+import edu.unibw.se.gdp.praktikum02.Geometrie;
+
+import java.awt.*;
+
+class Quader {
+    double cx;
+    double cy;
+    double dx;
+    double dy;
+    double ex;
+    double ey;
+    double abstand;
+    Color color;
+
+    public Quader(int hoehe, double ax, double ay, double bx, double by, double s, int i) {
+        cx = Geometrie.berechneCx(ay, bx, by);
+        cy = Geometrie.berechneCy(ax, bx, by);
+        dx = Geometrie.berechneDx(ax, ay, by);
+        dy = Geometrie.berechneDy(ax, ay, bx);
+        ex = Geometrie.berechneEx(dx, dy, cx, cy, s);
+        ey = Geometrie.berechneEy(dx, dy, cx, cy, s);
+        abstand = Math.hypot((bx-ax), (by-ay));
+
+        color = ((abstand <= 2.0 && i >=2) | (hoehe == 0 && i==1)) ? (SimpleGraphicPanel.getRandomColor()) : (Color.black);
+
+        SimpleGraphicPanel.drawQuadrilateral(color, true, ax, ay, bx, by, cx, cy, dx, dy);
+    }
+}
